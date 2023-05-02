7340e7e0ea8192e6400e087a6ea69f21e8b15a9f
Izentropowy <119621854+Izentropowy@users.noreply.github.com>
2023-05-02 16:41:02 +0200

    Add functions for distance, time and fuel calc

--- departure.js
+++ departure.js
@@ -44,7 +44,7 @@ function calcTorAndTod(pressAlt, temperature)
             [7000, 0, 1645, 2910],
             [8000, 0, 1820, 3265] ],
     
-        [   [   0, 10,  925, 1575],
+        [   [0, 10,  925, 1575],
             [1000, 10, 1010, 1720],
             [2000, 10, 1110, 1890],
             [3000, 10, 1215, 2080],
@@ -54,7 +54,7 @@ function calcTorAndTod(pressAlt, temperature)
             [7000, 10, 1785, 3170],
             [8000, 10, 1970, 3575] ],
     
-        [   [   0, 20,  995, 1690],
+        [   [0, 20,  995, 1690],
             [1000, 20, 1090, 1850],
             [2000, 20, 1195, 2035],
             [3000, 20, 1310, 2240],
@@ -64,7 +64,7 @@ function calcTorAndTod(pressAlt, temperature)
             [7000, 20, 1920, 3440],
             [8000, 20, 2120, 3880] ],
     
-        [   [   0, 30, 1070, 1810],
+        [   [0, 30, 1070, 1810],
             [1000, 30, 1170, 1990],
             [2000, 30, 1285, 2190],
             [3000, 30, 1410, 2420],
@@ -74,7 +74,7 @@ function calcTorAndTod(pressAlt, temperature)
             [7000, 30, 2065, 3730],
             [8000, 30, 2280, 4225] ],  
     
-        [   [   0, 40, 1150, 1945],
+        [   [0, 40, 1150, 1945],
             [1000, 40, 1260, 2135],
             [2000, 40, 1380, 2355],
             [3000, 40, 1515, 2605],
@@ -111,7 +111,7 @@ function calcTorAndTod(pressAlt, temperature)
 function calcRoc(pressAltTakeoff, pressAltCruise, temperature){
     let table = [
         [ 
-            [   0, -20,  855],
+            [0, -20,  855],
             [2000, -20, 760],
             [4000, -20, 685],
             [6000, -20, 575],
@@ -119,7 +119,7 @@ function calcRoc(pressAltTakeoff, pressAltCruise, temperature){
             [10000, -20, 360], 
             [12000, -20, 255] ],
     
-        [   [   0, 0,  785],
+        [   [0, 0,  785],
             [2000, 0, 695],
             [4000, 0, 620],
             [6000, 0, 515],
@@ -127,7 +127,7 @@ function calcRoc(pressAltTakeoff, pressAltCruise, temperature){
             [10000, 0, 300], 
             [12000, 0, 195] ],
     
-        [   [   0, 20,  710],
+        [   [0, 20,  710],
             [2000, 20, 625],
             [4000, 20, 555],
             [6000, 20, 450],
@@ -135,7 +135,7 @@ function calcRoc(pressAltTakeoff, pressAltCruise, temperature){
             [10000, 20, 240], 
             [12000, 20, 135]  ],
     
-        [   [   0, 40, 645],
+        [   [0, 40, 645],
             [2000, 40, 560],
             [4000, 40, 495],
             [6000, 40, 390],
@@ -167,6 +167,84 @@ function calcRoc(pressAltTakeoff, pressAltCruise, temperature){
         return roc;
 }
 
-calcRoc(0, 3000, 20);
+function calcTime(pressAltTakeoff, pressAltCruise, roc){
+    let height = pressAltCruise - pressAltTakeoff;
+    let time = Math.round(height / roc);
+    return time;
+}
 
-funct
+function calcCruiseDensity(qnh, elevation, cruise, temperature){
+    let p = qnh - 1 / 27 * (cruise - elevation);
+    let T = temperature - 2 / 1000 * (cruise - elevation);
+    let rho = (p*100) / (287 * (T+273));
+    return rho;
+}
+
+function calcTas(ias, rho){
+    let tas = ias / Math.sqrt(rho / 1.225);
+    return tas;
+}
+
+function calcClimbTime(qnh, elevation, cruise, temperature){
+    let pressAltTakeoff = calcPressAlt(qnh, elevation);
+    let pressAltCruise = pressAltTakeoff + cruise - elevation;
+    let roc = calcRoc(pressAltTakeoff, pressAltCruise, temperature);
+    let time = Math.round((cruise - elevation) / roc);
+    return time
+}
+
+function calcClimbDistance(qnh, elevation, cruise, temperature,){
+    // avg pressAlt for climb is 2/3 of the difference
+    let altClimb = 0.667 * (cruise - elevation);
+    // optimum IAS for climb is 73kt
+    let rho = calcCruiseDensity(qnh, elevation, altClimb, temperature);
+    let tas = calcTas(73, rho);
+
+    let time = calcClimbTime(qnh, elevation, cruise, temperature);
+    let distance = tas * time;
+    return distance;
+}
+
+function calcClimbFuel(qnh, elevation, cruise, temperature,){
+    let table = [
+        [0, 0],
+        [1000, 0.4],
+        [2000, 0.8],
+        [3000, 1.2],
+        [4000, 1.5],
+        [5000, 1.9],
+        [6000, 2.2],
+        [7000, 2.6],
+        [8000, 3.0],
+        [9000, 3.4],
+        [10000, 3.9],
+        [11000, 4.4], 
+        [12000, 5.0]];
+
+    let pressAltTakeoff = calcPressAlt(qnh, elevation);
+    let pressAltCruise = pressAltTakeoff + cruise - elevation;
+
+    let pressAltTakeoffFloor = Math.floor(pressAltTakeoff / 1000) * 1000;
+    let pressAltTakeoffCeil = Math.ceil(pressAltTakeoff / 1000) * 1000;
+
+    let pressAltCruiseFloor = Math.floor(pressAltCruise / 1000) * 1000;
+    let pressAltCruiseCeil = Math.ceil(pressAltCruise / 1000) * 1000;
+
+    let takeoffTable = table.reduce((acc, val) => {
+        if (val[0] === pressAltTakeoffFloor) acc.push(val);
+        if (val[0] === pressAltTakeoffCeil) acc.push(val);
+        return acc;
+    },[]);
+    let cruiseTable = table.reduce((acc, val) => {
+        if (val[0] == pressAltCruiseFloor || val[0] == pressAltCruiseCeil) acc.push(val);
+        if (val[0] == pressAltCruiseCeil) acc.push(val);
+        return acc;
+    },[]);
+
+    let takeoffFuel = interpolate(takeoffTable[0][0], takeoffTable[1][0], takeoffTable[0][1], takeoffTable[1][1], pressAltTakeoff);
+    let cruiseFuel = interpolate(cruiseTable[0][0], cruiseTable[1][0], cruiseTable[0][1], cruiseTable[1][1], pressAltCruise);
+    let fuel = cruiseFuel - takeoffFuel + 1.4;
+    return fuel;
+}
+
+console.log(calcClimbFuel(1013, 0, 000, 15));
